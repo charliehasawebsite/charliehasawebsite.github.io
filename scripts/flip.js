bb39e40e945df00a0ecae228a2606548965c1750
charliehasawebsite <105081822+charliehasawebsite@users.noreply.github.com>
2022-05-06 23:00:49 -0700

    Update flip.js

--- scripts/flip.js
+++ scripts/flip.js
@@ -1,9 +1,13 @@
+let container = document.getElementById("name-container");
 let letters = document.getElementsByClassName("letter");
 let subtitle  = document.getElementById("name-subtitle");
 function eraseName() {
 	for(let i = 0; i < letters.length; i++) {
+		container.style.transition = "none";
+		container.style.backgroundColor = "rgba(0, 0, 0, 0)";
 		letters[i].style.transition = "none";
 		letters[i].style.paddingTop = "10px";
+		letters[i].style.paddingBottom = "0";
 		letters[i].style.opacity = "0";
 		letters[i].style.transform = "rotateY(270deg)";
 	}
@@ -12,8 +16,11 @@ function animateName() {
 	setTimeout(() => {
 		for(let i = 0; i < letters.length; i++) {
 			setTimeout(() => {
-				letters[i].style.transition = "padding-top .5s, opacity .5s, transform .5s";
+				container.style.transition = "background-color 1s";
+				container.style.backgroundColor = "rgba(0, 0, 0, .35)";
+				letters[i].style.transition = "padding-top .5s, padding-bottom .5s, opacity .5s, transform .5s";
 				letters[i].style.paddingTop = "0";
+				letters[i].style.paddingBottom = "10px";
 				letters[i].style.opacity = "1";
 				letters[i].style.transform = "rotateY(360deg)";
 			}, 50*i); 
